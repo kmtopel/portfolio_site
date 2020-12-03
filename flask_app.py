e985500e6960bd9f65257074496a0ba8ea42698e
Kevin Topel <kmtopel@gmail.com>
2020-12-03 20:49:33 +0000

    created basic website structure

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
+import json
+
+github_data = json.loads(requests.get("https://api.github.com/users/kmtopel/repos").text)
+
+for i in github_data:
+    print(i['name'])
+    print(i['url'])
+    print(i['description'])
+    print('\n')
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Flask!'
+    return render_template('main.html',data=github_data)
 
